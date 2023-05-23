3fa78b9ab0a6ce094fcad2ab4985a415bbe4c100
SmilnitskiyStas <stassmilnitskiy@gmail.com>
2023-05-23 23:31:39 +0300

    Create New Project for OOP and StyleCop file

---
+++ Modul1_HomeWork1/Modul1_HomeWork1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul1_HomeWork1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
